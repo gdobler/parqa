1cf564200e1bdd03b4cc115f42a810e7e65cc990
Ouafa <ob465@nyu.edu>
2015-04-16 01:03:01 -0400

    added the plots and modified the csv files by removing unrecognized characters

--- PIPFailureCoincedences.py
+++ PIPFailureCoincedences.py
@@ -1,6 +1,8 @@
 from sys import argv
 import csv
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 f = open(argv[1], 'rU')
 myFile = csv.reader(f)
@@ -9,10 +11,14 @@ InspectionData = []
 InspectionSummary = {}
 InspectionItems = myFile.next()
 
+
+# Creating a list of Arrays to llop through the data instead of looping through the file.
 for row in myFile:
 	InspectionData.append(row)
 
 
+# Creating a dictionary of dctionaries where the key is one failing inspection feature
+# at a time measured against the rest failing features -- measuring coincidence 
 for i, item in enumerate(InspectionItems):
 	InspectionSummary[item] = {'Count':0}
 	for row in InspectionData:
@@ -25,10 +31,35 @@ for i, item in enumerate(InspectionItems):
 					else:
 						InspectionSummary[item][InspectionItems[j]] += 1
 
+
+# Pretty Printing!
+featureCounts = []
 for item in InspectionSummary:
 	if item != "Inspection_Id":
 		print "%s: %d fails" % (item, InspectionSummary[item]['Count']),
 		for feature in InspectionSummary[item]:
+			featureCounts.append(InspectionSummary[item][feature])
 			if feature != 'Count':
 				print "\t%s:%d" % (feature, InspectionSummary[item][feature]),
 		print ""
+
+
+
+
+# Plotting Histograms
+fig, axs = plt.subplots(1,len(InspectionSummary)-1, facecolor='w', edgecolor='k')
+fig.subplots_adjust(hspace = .5, wspace=.001)
+axs = axs.ravel()
+yLimMax= max(featureCounts)
+i = 0
+for item, features in InspectionSummary.iteritems():
+	if item != "Inspection_Id":
+		axs[i].bar(range(len(features)), features.values(), align='center', width=0.35)
+		axs[i].set_xticks(np.arange(0, len(features)+1, 1.0))
+		axs[i].set_xticklabels((features.keys()),rotation=90)
+		axs[i].set_ylim(0, yLimMax)
+		axs[i].set_title(item)
+		i+=1
+
+fig.suptitle(argv[1])
+plt.show()
