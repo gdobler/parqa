9d3345678e58c1b6c046a24da0e730e6642a2dc2
inaflash80 <inaflash80@gmail.com>
2015-04-21 18:36:14 -0400

    updated failure metric after speaking with Greg

--- PIPMasterFailedCoincidences.py
+++ PIPMasterFailedCoincidences.py
@@ -5,54 +5,67 @@ import numpy as np
 import math
 
 Inspections = {}
+
 Features = {}
 
-# IE. Features = {Ice: {Count:15, Failures: {Sidewalks:10, Playgrounds:12}}, 
+# IE. Inspection = {IDNumber: [(Feature1, 1), (Feature2, 0)]}
+
+
+# IE. Features = {Ice: {Count:15, Failures: {Sidewalks:{EvaluatedCount:10, Failures:10}, Playgrounds:12}}, 
 #		  		  Litter: {Count:25, Failures: {Sidewalks:10, Playgrounds,14}}}
 
 data = pd.ExcelFile(sys.argv[1])
 df = data.parse()
 
 # All failures
-failedFeatures = df[df['Rating'].isin(['U'])]
+# inspectionFeatures = df[df['Rating'].isin(['U'])]
 # print failedFeatures
 
-for feature in failedFeatures.iterrows():
+for feature in df.iterrows():
 	featureName = feature[1][0]
+	featureRating = feature[1][1]
 	inspectionID = feature[1][2]
 
 	# Initialize Features in Master Dictionary
-	if Features.get(featureName) == None:
-		Features[featureName] = {'Count':0, 'Failures':{}}
+	if featureRating == 'U':
+		if Features.get(featureName) == None:
+			Features[featureName] = {'Count':0, 'Failures':{}}
+			
+		# Increment Feature Count
+		Features[featureName]['Count'] += 1
 
-	# Initialize Inspection in Master Inspection Dictionary
+		# Initialize Inspection in Master Inspection Dictionary
 	if Inspections.get(inspectionID) == None:
 		Inspections[inspectionID]= []
 
-	# Increment Feature Count
-	Features[featureName]['Count'] += 1
-
 	# Add Feature to corresponding Inspection 
-	Inspections[inspectionID].append(featureName)
+	Inspections[inspectionID].append((featureName, featureRating))
 
 for inspection in Inspections:
 	# print inspection
 	for i, feature in enumerate(Inspections[inspection]):
-		for compareFeature in Inspections[inspection][i+1:]:
-			if Features[feature]['Failures'].get(compareFeature) == None:
-				Features[feature]['Failures'][compareFeature] = 0
-			Features[feature]['Failures'][compareFeature] += 1
+		if feature[1] == 'U':
+			for compareFeature in Inspections[inspection][i+1:]:
 
-			if Features[compareFeature]['Failures'].get(feature) == None:
-				Features[compareFeature]['Failures'][feature] = 0
-			Features[compareFeature]['Failures'][feature] += 1
+				if Features[feature[0]]['Failures'].get(compareFeature[0]) == None:
+					Features[feature[0]]['Failures'][compareFeature[0]] = {'EvaluatedCount':0, 'Failures':0}
+				# Increment Evaluated count regardless of that compared Feature's rating
+				Features[feature[0]]['Failures'][compareFeature[0]]['EvaluatedCount'] += 1
+				# Increment Failed count if that compared Feature has a failed rating
+				if compareFeature[1] == 'U':
+					Features[feature[0]]['Failures'][compareFeature[0]]['Failures'] += 1
 
-print Features
+					if Features[compareFeature[0]]['Failures'].get(feature[0]) == None:
+						Features[compareFeature[0]]['Failures'][feature[0]] = {'EvaluatedCount':0, 'Failures':0}
+					Features[compareFeature[0]]['Failures'][feature[0]]['EvaluatedCount'] += 1
+					Features[compareFeature[0]]['Failures'][feature[0]]['Failures'] += 1
+
 
 for feature in Features:
 	print "%s: %d fails" % (feature, Features[feature]['Count'])
 	for subFeature in Features[feature]['Failures']:
-		print "\t%s:%.4f" % (subFeature, float(Features[feature]['Failures'][subFeature]) / Features[feature]['Count']) # ratio is printed
+		differenceInEvaluated = Features[feature]['Failures'][subFeature]['EvaluatedCount'] - Features[feature]['Failures'][subFeature]['Failures']
+		print "\t%s:%.4f Count: %d, Evaluated: %d" % (subFeature, float(Features[feature]['Failures'][subFeature]['Failures']) / Features[feature]['Failures'][subFeature]['EvaluatedCount'], Features[feature]['Failures'][subFeature]['Failures'], Features[feature]['Failures'][subFeature]['EvaluatedCount']) # ratio is printed
 	print ""
 
 
