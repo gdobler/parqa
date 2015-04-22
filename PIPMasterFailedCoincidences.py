9eaba581d67940e724613bc7e70cbed7f918958e
Ouafa <ob465@nyu.edu>
2015-04-22 12:13:03 -0400

    added code for creating the heatmap, and created a folder for outputs

--- PIPMasterFailedCoincidences.py
+++ PIPMasterFailedCoincidences.py
@@ -68,22 +68,52 @@ for feature in Features:
 	print ""
 
 
+featuresList = [u'Litter', u'Athletic Fields', u'Lawns', u'Safety Surface', u'Trails', u'Weeds', u'Sidewalks', u'Ice', u'Glass', u'Benches', u'Paved Surfaces', u'Graffiti', u'Trees', u'Water Bodies', u'Play Equipment', u'Fences', u'Horticultural Areas']
+newList = []
+for index, feature in enumerate(featuresList):
+	newList.append([])
+	for i in featuresList:
+		if i == feature:
+			newList[index].append (1)
+		elif i == u'Weeds' and feature == u'Ice':
+			newList[index].append (0)
+		elif i == u'Safety Surface' and feature == u'Water Bodies':
+			newList[index].append (0)
+		elif i == u'Ice' and feature == u'Weeds':
+			newList[index].append (0)
+		elif i != feature:
+			newList[index].append (float(Features[feature]['Failures'][i]['Failures'])/Features[feature]['Failures'][i]['EvaluatedCount'])
 
-
-fig, axs = plt.subplots(1,len(Features), facecolor='w', edgecolor='k')
-fig.subplots_adjust(hspace = .5, wspace=.001)
-axs = axs.ravel()
-i = 0
-for mainFeature, details in Features.iteritems():
-	for main, features in details.iteritems():
-		if main != 'Count':
-			axs[i].bar(range(len(features)), [float(x) / details['Count'] for x in features.values()], align='center', width=0.35)
-			axs[i].set_xticks(np.arange(0, len(features)+1, 1.0))
-			axs[i].set_xticklabels((details['Failures'].keys()),rotation=90)
-			axs[i].set_ylim(0, 1)
-			axs[i].set_title('%s, count of : %d'%(mainFeature, details['Count']))
-			i+=1
-fig.suptitle('Pecentages of Coincedences')
+newList = np.array(newList)
+plt.imshow(newList, interpolation='nearest')
+plt.xticks(range(17),featuresList, rotation='vertical')
+plt.yticks(range(17),featuresList)
+plt.colorbar()
+plt.title('Coincidence Ratios of Failing Features')
+plt.gcf().subplots_adjust(bottom=0.20)
 plt.show()
+
+
+
+##### Previous code for plotting
+# fig, axs = plt.subplots(4,4, facecolor='w', edgecolor='k')
+# fig.subplots_adjust(hspace = .5, wspace=.001)
+# axs = axs.ravel()
+# i = 0
+# for mainFeature, details in Features.iteritems():
+# 		for detailType, failureDetails in details.iteritems():
+# 			if detailType != 'Count':
+# 				print failureDetails
+# 				print "++++++++++++"
+# 			for InspectionFailure, EvCount in failureDetails.iteritems():
+# 		if main != 'Count':
+# 			axs[i].bar(range(len(features)), [float(x) / details['Count'] for x in features.values()], align='center', width=0.35)
+# 			axs[i].set_xticks(np.arange(0, len(features)+1, 1.0))
+# 			axs[i].set_xticklabels((details['Failures'].keys()),rotation=90)
+# 			axs[i].set_ylim(0, 1)
+# 			axs[i].set_title('%s, count of : %d'%(mainFeature, details['Count']))
+# 			i+=1
+# fig.suptitle('Pecentages of Coincedences')
+# plt.show()
 
 
