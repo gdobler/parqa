e042c5bac7c246f588f537c180ec107ecea0d81e
inaflash80 <inaflash80@gmail.com>
2015-04-27 21:09:32 -0400

    Restructured internal dictionaries and minimized iterations amongst datasets

--- Park_Quality_Ratings.py
+++ Park_Quality_Ratings.py
@@ -3,6 +3,11 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import datetime
+
+# Global Dictionarys
+parkInfo = {}
+Inspections = {}
 
 PIP_InspectionMain = pd.ExcelFile("InspectionMain.xlsx")
 inspection = PIP_InspectionMain.parse()
@@ -16,16 +21,24 @@ sites = PIP_AllSites.parse()
 failedFeatures = rating[rating['Rating'].isin(['U'])]
 badFeatures = rating[rating['Rating'].isin(['U/S'])]
 
+'''
+Example Structures:
 
-# In[ ]:
+parkID = {<PARKID>: 
+            {'Acres':0, 'Name':0, 'Borough':0, 'District':0, 'Category':0, 'Inpections':
+                                                                                {<INSPECTIONID> : {'Count':0, 'Failures':0, 'Date':date, 'Rating':0}},
+                                                                                {<INSPECTIONID> : {'Count':0, 'Failures':0, 'Date':date, 'Rating':0}},
+                                                                                {<INSPECTIONID> : {'Count':0, 'Failures':0, 'Date':date, 'Rating':0}}
+            }
+          }
 
 
 
+'''
 
-# In[2]:
 
-Inspections = {}
-
+#  Iterate through all inspection reports and sum Ratio of Failures of features to Features themselves.
+#  Maintain 'Inspection' dictionary of format Inspection[<INSPECTIONID>]: {'Count':X, 'Failures':X}
 for feature in rating.iterrows():
     inspectionID = feature[1][2]
     featureRating =feature[1][1]
@@ -39,30 +52,6 @@ for feature in rating.iterrows():
     Inspections[inspectionID]['Count']+=1
 
 
-# In[ ]:
-
-
-
-
-# In[7]:
-
-Quality = {}
-Ratio = 0
-
-for key, value in Inspections.iteritems():
-    if Quality.get(key) == None:
-        Quality[key] = Ratio
-    for item, score in value.iteritems():
-        if item == 'Count':
-            Count = score
-        if item == 'Failures':
-            Fail = score
-    Ratio = float(Fail)/float(Count)
-
-
-# In[8]:
-
-parkInfo = {}
 for feature in sites.iterrows():
     parkID = feature[1][1]
     borough = feature[1][2]
@@ -72,7 +61,7 @@ for feature in sites.iterrows():
     category = feature[1][10]
     
     if parkInfo.get(parkID) == None:
-        parkInfo[parkID] = {'Acres':0, 'Name':0, 'Borough':0, 'District':0, 'Category':0, 'Inpections':{}}
+        parkInfo[parkID] = {'Inpections':{}}
     parkInfo[parkID]['Acres']=float(acres)
     parkInfo[parkID]['Name']=name
     parkInfo[parkID]['Borough']=borough
@@ -80,84 +69,45 @@ for feature in sites.iterrows():
     parkInfo[parkID]['Category']=category
 
 
-            
-            #parkInfo[parkID]['Inpections']={inspectionID:{'Date':0,'Rating':0}}
-    
-    #if parkInfo.get(parkID) == None:
-     #   pass
-    #else:
-       # if parkInfo[parkID] == parkID:
-        #    parkInfo[parkID]['Inpections']={inspectionID:{'Date':0,'Rating':0}}
-    #parkInfo[inspectionID]['Date']=date
-    
-    
-
-
-# In[30]:
-
-parkInspections = {}
-import datetime
 for feature in inspection.iterrows():
     parkID = feature[1][0]
     date = feature[1][3]
     inspectionID = feature[1][11]
-    
-    if parkInspections.get(parkID) == None:
-        parkInspections[parkID] = {inspectionID:{'Date':date,'Rating':0}}
-    #parkInspections[parkID][inspectionID]['Date']= date
 
-    for key, value in Quality.iteritems():
-        if inspectionID == key:
-            parkInspections['Rating']=value
-            
+    '''  SEE if we can just read off the dataframe instead of going through the above for loop
+    # Read entry 0... just in case there is more than one entry per parkID.. There shouldn't be regardless
+    parkDfEntry = sites[sites[1] == parkID].ix[0]
+    if parkDfEntry.empty:
+      print 'No parkID matches %s found in Inspection file. SKIPPING inspectionID %s!' % (parkID, inspectionID)
+    else:
+      if parkInfo.get(parkID) == None:
+          parkInfo[parkID] = {'Inpections':{}}
+      
+      parkInfo[parkID]['Acres']=float(parkDfEntry[1][9])
+      parkInfo[parkID]['Name']=parkDfEntry[1][6]
+      parkInfo[parkID]['Borough']=parkDfEntry[1][2]
+      parkInfo[parkID]['District']=parkDfEntry[1][4]
+      parkInfo[parkID]['Category']=parkDfEntry[1][10]
 
-###
-#for key, values in parkInfo.items():
- #   for feature in inspection.iterrows():
-#      parkID = feature[1][0]
-   #     date = feature[1][3]
-    #    inspectionID = feature[1][11]    
-     #   if key == parkID:
-    #     parkInfo[parkID]['Inpections']={inspectionID:{'Date':0,'Rating':0}}
-       #     print parkInfo
+      IF USING ABOVE, INDENT BELOW!
+          '''
+      
+    if parkInfo.get(parkID) == None:
+      print 'Error: parkID %s not found in SITES file' % parkID
+    else if Inspection.get(inspectionID) == None:
+      print 'Error: inspectionID %s not found in FEATURERATING file' % inspectionID
+    else:
+      # Add inspection report to ParkID Dictionary
+      parkInfo[parkID]['Inspection'][inspectionID] = Inspection[inspectionID]
 
+      # Add inspection Date to Inspection sub Dictionary
+      parkInfo[parkID]['Inspection'][inspectionID]['Date'] = date    # CHANGE TO DATETIME FORMAT!!!
 
-# In[31]:
+      # Create Ratio for inspection report and add to Inspection sub Dictionary
+      inspectionFeatureCount = parkInfo[parkID]['Inspection'][inspectionID]['Count']
+      inspectionFailureCount = parkInfo[parkID]['Inspection'][inspectionID]['Failures']
+      parkInfo[parkID]['Inspection'][inspectionID]['Ratio'] = float(inspectionFailureCount)/inspectionFeatureCount
+      
 
-#parkInspections
-
-
-# In[9]:
-
-#QualityRating = {}
-
-#for feature in inspection.iterrows():
-#    inspectionIDMaster = feature[1][11]
-#    Date = feature[1][3]
-#    Prop_ID = feature[1][0]
-
- #   if QualityRating.get(inspectionIDMaster) == None:
-  #      QualityRating[inspectionIDMaster] = {'Prop_ID':0, 'Date':0, 'Rating':0, 'Area':0}
-   # QualityRating[inspectionIDMaster]['Prop_ID']=Prop_ID
-  #  QualityRating[inspectionIDMaster]['Date']=Date
-#for key,value in Quality.iteritems():
-    #for ID,values in QualityRating.iteritems():
-      #  if key == ID:
-        #    QualityRating[inspectionIDMaster]['Rating']=float(value)
-
-
-# In[10]:
-
-#for feature in sites.iterrows():
- #   Prop_ID = feature[1][2]
- #   featureRating =feature[1][1]
-
-
-# In[11]:
-
-#for key,value in Quality.iteritems():
-   # for ID,values in QualityRating.iteritems():
-     #   if key == ID:
-       #     QualityRating[inspectionIDMaster]['Rating']=float(value)
-            
-            ""
+# Print a confirmation example of a park reference:
+print parkInfo[parkID]
